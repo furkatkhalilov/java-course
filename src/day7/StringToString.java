4ed1ed553e73401f7916d54181272ae6ef3b1901
TechnoStudy <info@techno.study>
2020-03-24 12:51:27 -0400

    Day 7

---
+++ src/day7/StringToString.java
@@ -0,0 +1,22 @@
+package day7;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
+public class StringToString {
+
+    /*
+     toString()
+
+     Returns the value of a String object
+
+     String
+     */
+
+    public static void main(String[] args) {
+        String text = "Some cool text";
+
+        System.out.println( text.toString() );
+    }
+}
