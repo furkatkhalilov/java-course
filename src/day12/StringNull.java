93aa50ba39581f8dc124158671a55262a4d9e2d3
TechnoStudy <info@techno.study>
2020-03-31 12:48:56 -0400

    Day 12

---
+++ src/day12/StringNull.java
@@ -0,0 +1,13 @@
+package day12;
+
+import java.util.Scanner;
+
+public class StringNull {
+
+    static String s;
+
+    public static void main(String[] args) {
+
+        System.out.println(s);
+    }
+}
