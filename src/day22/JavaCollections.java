c768bf6e25a9aa7c2da10ab535b74112a314ca65
TechnoStudy <info@techno.study>
2020-04-16 12:59:56 -0400

    Day 21

---
+++ src/day22/JavaCollections.java
@@ -0,0 +1,16 @@
+package day22;
+
+public class JavaCollections {
+
+    public static void main(String[] args) {
+//        Collections.sort();
+
+//        Collections.fill();
+
+//        Collections.max()
+
+//        Collections.min()
+
+//        Collections.reverse();
+    }
+}
