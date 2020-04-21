32bc77b6d6ff3e3315cb0ebcd6695c400bd4f3ef
TechnoStudy <info@techno.study>
2020-04-21 19:01:05 -0400

    Day 24

--- src/day23/Task2.java
+++ src/day23/Task2.java
@@ -29,12 +29,35 @@ public class Task2 {
     }
 
     //3. check if you have duplication in you array
-    public static boolean checkDuplication(String[] names){
-        return false;
-    }
+    public static boolean hasDuplication(String[] names) {
+        HashSet<String> namesSet = convertToHashSet(names);
+        int setSize = namesSet.size();
+        int arraySize = names.length;
 
+        return arraySize != setSize;
+    }
 
     //4. check if you have name "Ali" in you set/list,
     // if you have, print he is best mentor ever
     // else, print I wish we had him
+    public static boolean hasName(HashSet<String> set, String name) {
+        return set.contains(name);
+    }
+
+    public static void checkName(HashSet<String> set) {
+        if(hasName(set, "Ali")) {
+            System.out.println("He is best mentor ever!!!");
+        } else {
+            System.out.println("I wish we had him");
+        }
+
+        // 2. way
+        System.out.println(
+                hasName(set, "Ali")
+                        ? "He is best mentor ever!!!"
+                        : "I wish we had him"
+        );
+
+    }
+
 }
