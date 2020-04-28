6737a25cedb6314b30f85eba76f2eab45e334754
TechnoStudy <info@techno.study>
2020-04-28 12:52:16 -0400

    Day 28 Task 2

--- src/day28/tasks/task2/Task2.java
+++ src/day28/tasks/task2/Task2.java
@@ -6,7 +6,11 @@ public class Task2 {
         Rectangle rectangle1 = new Rectangle();
         rectangle1.width = 10;
         rectangle1.length = 5;
+
         String output = "Perimeter: " + rectangle1.getPerimeter();
         System.out.println(output);
+        System.out.println("Area: " + rectangle1.getArea());
+        System.out.println(rectangle1);
     }
+    
 }
