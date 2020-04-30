e25a759890711119b4e6c6039c6ff4da7cb55c62
TechnoStudy <info@techno.study>
2020-04-30 11:12:00 -0400

    Day 29 Task 1

--- src/day29/tasks/task1/Task1.java
+++ src/day29/tasks/task1/Task1.java
@@ -4,6 +4,11 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        Lesson math = createLesson("Math", 4, 96);
+        Lesson physics = createLesson("Physics", 4, 95);
+        Lesson science = createLesson("Science", 3, 100);
+        Lesson computerScience = createLesson("Computer Science", 4, 100);
+        Lesson chemistry = createLesson("Chemistry", 3, 95);
 
 
         Student student1 = new Student();
@@ -15,4 +20,12 @@ public class Task1 {
 
     }
 
+    public static Lesson createLesson(String name, int credit, double grade) {
+        Lesson newLesson = new Lesson();
+        newLesson.name = name;
+        newLesson.credit = credit;
+        newLesson.grade = grade;
+
+        return newLesson;
+    }
 }
