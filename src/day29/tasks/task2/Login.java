6905c3c8d8bcbc2393291cbfc5d28f8365b93334
TechnoStudy <info@techno.study>
2020-04-30 12:27:12 -0400

    Day 29 Task 2: Part 1

---
+++ src/day29/tasks/task2/Login.java
@@ -0,0 +1,13 @@
+package day29.tasks.task2;
+
+/*
+***Part 1***
+create class Login with attributes
+1. username
+2. password
+ */
+public class Login {
+    String username;
+    String password;
+
+}
