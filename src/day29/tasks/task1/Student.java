beb6bb8496e8264c3ee888ec46bc329b11772480
TechnoStudy <info@techno.study>
2020-04-30 11:09:29 -0400

    Day 29 Task 1

---
+++ src/day29/tasks/task1/Student.java
@@ -0,0 +1,8 @@
+package day29.tasks.task1;
+
+import java.util.ArrayList;
+
+public class Student {
+    String name;
+    ArrayList<Lesson> lessons = new ArrayList<>();
+}
---
+++ src/day29/tasks/task1/Task1.java
@@ -0,0 +1,18 @@
+package day29.tasks.task1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+
+
+        Student student1 = new Student();
+        student1.name = "John";
+
+        Student student2 = new Student();
+        student2.name = "Max";
+
+
+    }
+
+}
