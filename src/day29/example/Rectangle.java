307ce607516971c98f1a07bf0d35582072265b4b
TechnoStudy <info@techno.study>
2020-04-30 12:18:03 -0400

    Day 29 example

---
+++ src/day29/example/Rectangle.java
@@ -0,0 +1,13 @@
+package day29.example;
+
+public class Rectangle {
+
+    int length;
+    int width;
+
+    public int getArea() {
+//        return MyMath.abs(length) * MyMath.abs(width);
+        return length * width;
+    }
+
+}
