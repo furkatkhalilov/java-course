eb42b1f5e6804754b3da3ea82a0b09c88dd14e4d
TechnoStudy <info@techno.study>
2020-04-30 12:42:20 -0400

    Day 29 Task 2: Part 3

--- src/day29/tasks/task2/Login.java
+++ src/day29/tasks/task2/Login.java
@@ -9,5 +9,40 @@ create class Login with attributes
 public class Login {
     String username;
     String password;
+    String role;//admin, user
+
+    public static Login createAdmin(String u, String p) {
+        Login login = new Login();
+        login.username = u;
+        login.password = p;
+        login.role = "Admin";
+
+        return login;
+    }
+    public static Login createUser(String u, String p) {
+        Login login = new Login();
+        login.username = u;
+        login.password = p;
+        login.role = "User";
+
+        return login;
+    }
+
+    /*
+    ***Part 3***
+    create instance method in Login to check
+    if username and password are not empty and not null
+     */
+    public void check() {
+        boolean invalidUsername = username == null && username.isEmpty();
+        if(invalidUsername) {
+            System.out.println("user name is null or empty");
+        }
+
+        boolean invalidPassword = password == null && password.isEmpty();
+        if(invalidPassword) {
+            System.out.println(" passord is null or empty");
+        }
+    }
 
 }
