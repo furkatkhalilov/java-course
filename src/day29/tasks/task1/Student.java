f0f44723f1447bfeae8d9bec5961bee0cfdd8454
TechnoStudy <info@techno.study>
2020-04-30 11:47:23 -0400

    Day 29 Task 1

--- src/day29/tasks/task1/Student.java
+++ src/day29/tasks/task1/Student.java
@@ -7,17 +7,17 @@ public class Student {
     int maxCredit;
     ArrayList<Lesson> lessons = new ArrayList<>();
 
-    void takeLesson(Lesson subject) {
+    void takeLesson(Lesson lesson) {
         // calculate how many credits we have currently
         int currentTotalCredit = 0;
-        for(Lesson lesson : lessons) {
-            currentTotalCredit += lesson.credit;
+        for(Lesson lsn : lessons) {
+            currentTotalCredit += lsn.credit;
         }
 
         // control maximum credit that student can have
-        if(currentTotalCredit + subject.credit < maxCredit) {
-            lessons.add(subject);
-        }else {
+        if(currentTotalCredit + lesson.credit < maxCredit) {
+            lessons.add(lesson);
+        } else {
             System.out.println("You've already taken to many lessons!!!");
         }
 
