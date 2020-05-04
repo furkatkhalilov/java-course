f29548bc5e6a9d8c2bb181c085bd44e826b75848
TechnoStudy <info@techno.study>
2020-05-04 12:11:46 -0400

    Day 30 Task 1

--- src/day30/tasks/task1/Bank.java
+++ src/day30/tasks/task1/Bank.java
@@ -9,15 +9,26 @@ public class Bank {
     public Bank() {
         name = "Unknown";
     }
+
     // Second
     public Bank(String name, int serial) {
         this.name = name;
         this.serial = serial;
     }
+
     // Third
     public Bank(String name, int serial, boolean trustworthiness) {
         this(name, serial);
         this.trustworthiness = trustworthiness;
+    }
+
+    // Four, please, don't do it like this
+    public Bank(int serial) {
+        this("Bank four", serial, false);
+    }
+
+    public static Bank nonTrustworthyBank(String name, int serial) {
+        return new Bank(name, serial);
     }
 
     public String toString() {
--- src/day30/tasks/task1/Task1.java
+++ src/day30/tasks/task1/Task1.java
@@ -9,6 +9,7 @@ public class Task1 {
         System.out.println("Bank1: " + bank1);
 
         Bank bank2 = new Bank("Bank of America", 1010111);
+//        bank2 = Bank.nonTrustworthyBank("Bank of America", 1010111);
         System.out.println("Bank2: " + bank2);
 
         Bank bank3 = new Bank("Chase", 3344967, true);
@@ -17,5 +18,9 @@ public class Task1 {
         Bank bank4 = new Bank();
         bank4.name = "Halk Bank";
         System.out.println("Bank4: " + bank4);
+
+
+        Bank bank5 = new Bank(101010);
+        System.out.println("Bank5: " + bank5);
     }
 }
