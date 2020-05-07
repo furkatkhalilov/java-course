11bd4a6616813dc5992c15dd6732a7402972f993
TechnoStudy <info@techno.study>
2020-05-07 11:28:21 -0400

    Day 32 Non-Access Modifier: static attribute

--- src/day32/nonAccessModifier/staticModifier/Person.java
+++ src/day32/nonAccessModifier/staticModifier/Person.java
@@ -12,17 +12,18 @@ public class Person {
 
 
     public static void main(String[] args) {
-        Person.planetName = "Earth";
-
         Person person1 = new Person();
         person1.name = "John";
+        person1.planetName = "Earth";
         person1.print();
 
         Person person2 = new Person();
         person2.name = "Alice";
+        person2.planetName = "Earth";
         person2.print();
 
-        Person.planetName = "Mars";
+        person1.planetName = "Mars";
+        person2.planetName = "Mars";
         System.out.println("After expedition");
         person1.print();
         person2.print();
