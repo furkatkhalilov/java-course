95c6f0a1913f7edc5aebbe1f364ec09d442de513
TechnoStudy <info@techno.study>
2020-05-14 10:49:53 -0400

    Day 37 Checked & Unchecked exceptions

---
+++ src/day37/examples/ErrorEx.java
@@ -0,0 +1,13 @@
+package day37.examples;
+
+public class ErrorEx {
+    public static void main(String[] args) {
+
+        try{
+            //your code
+        }catch(Error er){
+
+        }
+
+    }
+}
