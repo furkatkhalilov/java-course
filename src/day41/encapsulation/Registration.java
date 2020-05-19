cfae52172033c62b112077cb87de3a607a72331f
TechnoStudy <info@techno.study>
2020-05-19 10:39:53 -0400

    Day 41 Encapsulation

--- src/day41/encapsulation/Registration.java
+++ src/day41/encapsulation/Registration.java
@@ -5,12 +5,14 @@ public class Registration {
     public static void main(String[] args) {
         User user = new User();
         //user.id = 10; instance field id is private, cannot assign value like this
-
 //        user.setId(10);
 //        user.setUsername("techno@study");
 //        user.setPassword("190520");
 
-        user = new User(5, "dos@techno","1993355");
+        ///user.password = "2321312";
+
+
+        user = new User(5, "dos@techno","1355");
 
 
         System.out.println("id: " + user.getId());
--- src/day41/encapsulation/User.java
+++ src/day41/encapsulation/User.java
@@ -10,14 +10,21 @@ public class User {
 
     }
 
+//    public User(long id, String username, String password) {
+//        setId(id);
+//        setUsername(username);
+//        setPassword(password);
+//    }
+
+
     public User(long id, String username, String password) {
         this.id = id;
         this.username = username;
-        this.password = password;
+        //this.password = password;
+        setPassword(password);
     }
+
     // to make write-only, remove all get methods
-
-
     public void setId(long id) {
         this.id = id;
     }
@@ -27,20 +34,31 @@ public class User {
     }
 
     public void setPassword(String password) {
+        //password is valid only when length > 6
+        if(password.length() < 6) {
+            throw new IllegalArgumentException("Password length is too short, provide longer password!");
+        }
+
+        //
         this.password = password;
     }
 
     // to make read-only, remove all set methods
+
+
     public long getId() {
-        return this.id;
+        return id;
     }
 
     public String getUsername() {
-        return this.username;
+        return username;
     }
 
+    // assume if id = 1, it is ADMIN, and we do not show password of admin
     public String getPassword() {
-        return this.password;
+        if(this.id == 1){
+            throw new RuntimeException("you cannot see password of admin");
+        }
+        return password;
     }
-
 }
