ca5de96152dbab7924d18ffa7b5e2bab9b26ad8e
TechnoStudy <info@techno.study>
2020-05-26 12:50:01 -0400

    Day 43 task2

---
+++ src/day43/tasks/task2/GeometryApp.java
@@ -0,0 +1,8 @@
+package day43.tasks.task2;
+
+public class GeometryApp {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(3);
+        System.out.println(circle1);
+    }
+}
