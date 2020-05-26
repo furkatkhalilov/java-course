15ba3a91ea266ab1428d5d08d0c9a8b70935a01a
TechnoStudy <info@techno.study>
2020-05-26 11:07:35 -0400

    Day 43 task1

--- src/day43/extendsKeyword/ex1/SubClass.java
+++ src/day43/extendsKeyword/ex1/SubClass.java
@@ -1,5 +1,6 @@
 package day43.extendsKeyword.ex1;
 
+//subclass sometimes child class, derived class
 public class SubClass extends SuperClass {
 
     public String toString() {
--- src/day43/extendsKeyword/ex1/SuperClass.java
+++ src/day43/extendsKeyword/ex1/SuperClass.java
@@ -1,5 +1,6 @@
 package day43.extendsKeyword.ex1;
 
+//superclass sometimes called parent class
 public class SuperClass {
 
     public String name;
