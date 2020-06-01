35191f8ec6b0ad52edcc6db02121e9e7d0dd7a6e
TechnoStudy <info@techno.study>
2020-06-01 12:07:39 -0400

    Day 46 Task 2

--- src/day46/tasks/task2/Animal.java
+++ src/day46/tasks/task2/Animal.java
@@ -1,6 +1,13 @@
 package day46.tasks.task2;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public abstract class Animal {
+    private long id;
+    private String name;
+    private boolean wild;
+    private LocalDate dob;
 
     //what is favorite meal
     public abstract String food();
@@ -14,4 +21,48 @@ public abstract class Animal {
     //what sound does animal produce
     public abstract String greeting();
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isWild() {
+        return wild;
+    }
+
+    public void setWild(boolean wild) {
+        this.wild = wild;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return Period.between(dob, LocalDate.now()).getYears();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() +
+                "\nid=" + id +
+                "\nname=" + name +
+                "\nwild=" + wild +
+                "\ndob=" + dob;
+    }
 }
