d10d443285cf4f26e9c6367e2a190aa58b3ee1f2
TechnoStudy <info@techno.study>
2020-06-09 11:44:30 -0400

    Day 52 Custom StringSet

---
+++ src/day52/App.java
@@ -0,0 +1,18 @@
+package day52;
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        HashStringSet set = new HashStringSet();
+//        set.add("str");
+//        set.add("str");
+//        set.add("str");
+//        set.isEmpty();
+//        set.remove("str");
+//        set.size("str");
+//        set.clear();
+
+    }
+}
---
+++ src/day52/StringSet.java
@@ -0,0 +1,18 @@
+package day52;
+
+//Set is a part of collection keep the objects without duplication
+//collection of unique objects
+public interface StringSet {
+
+    void add(String s);
+
+    void remove(String s);
+
+    int size();
+
+    //remove all String from StringSet
+    void clear();
+
+    boolean isEmpty();
+
+}
