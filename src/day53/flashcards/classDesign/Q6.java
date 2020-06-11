4eb8f67072125b4924b6e3cdb9a7163d19308c16
TechnoStudy <info@techno.study>
2020-06-11 13:50:14 -0400

    Day 53

---
+++ src/day53/flashcards/classDesign/Q6.java
@@ -0,0 +1,19 @@
+package day53.flashcards.classDesign;
+
+public class Q6 {
+
+
+}
+
+class Spider {
+
+    public void printName(short input) {
+        System.out.print("A");
+    }
+
+
+    public static void main(String[] args) {
+        Spider spider = new Spider();
+        spider.printName((short) 4);
+    }
+}
